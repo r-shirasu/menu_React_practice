452bc7d10e5040a36c8faadc4b0d5a737e9219da
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-16 17:13:50 +0900

    タイトル部分まで作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import "./App.scss";
+import { Menu } from "./Menu";
 
 export const App = () => {
-  return <div className="App">Hello, world!</div>;
+  return (
+    <div className="menu">
+      <div className="title">
+        <h2>Our Menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Menu />
+    </div>
+  );
 };
