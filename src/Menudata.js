bfc6d8303243b29d8a1c7458923d8a44d67ed27e
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-06-17 15:38:26 +0900

    Menudata.jsに情報を格納し、Menu.jsでimportする

---
+++ src/Menudata.js
@@ -0,0 +1,10 @@
+export const menudata = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+];
